2a14526585958652a0422c8cd2df6db19c8411ae
Vardhan158 <harshavardhandevang@gmail.com>
2025-05-24 23:11:34 +0530

    Update EmployeeList.js

--- frontend/src/components/EmployeeList.js
+++ frontend/src/components/EmployeeList.js
@@ -8,7 +8,44 @@ const EmployeeList = () => {
       .then(res => res.json())
       .then(data => setEmployees(data))
       .catch(err => console.error('Error:', err));
+  }, []);import React, { useEffect, useState } from 'react';
+import '../common.css'; // make sure this is imported if using custom styles
+
+const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/employees')
+      .then(res => res.json())
+      .then(data => setEmployees(data))
+      .catch(err => console.error('Error:', err));
   }, []);
+
+  return (
+    <div className="employee-list-container">
+      <h2 className="employee-list-title">Employee List</h2>
+      <table className="employee-table">
+        <thead>
+          <tr>
+            <th>Employee ID</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map(emp => (
+            <tr key={emp.employeeId}>
+              <td>{emp.employeeId}</td>
+              <td>{emp.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default EmployeeList;
+
 
   return (
     <div className="p-4">
