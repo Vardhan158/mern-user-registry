8eb764cfc7c56292869f7061cb537357454bc74a
Vardhan158 <harshavardhandevang@gmail.com>
2025-05-24 23:16:09 +0530

    Update EmployeeList.js

--- frontend/src/components/EmployeeList.js
+++ frontend/src/components/EmployeeList.js
@@ -1,14 +1,4 @@
 import React, { useEffect, useState } from 'react';
-
-const EmployeeList = () => {
-  const [employees, setEmployees] = useState([]);
-
-  useEffect(() => {
-    fetch('https://backend-82bb.onrender.com/api/employees')
-      .then(res => res.json())
-      .then(data => setEmployees(data))
-      .catch(err => console.error('Error:', err));
-  }, []);import React, { useEffect, useState } from 'react';
 import '../common.css'; // make sure this is imported if using custom styles
 
 const EmployeeList = () => {
@@ -36,32 +26,6 @@ const EmployeeList = () => {
             <tr key={emp.employeeId}>
               <td>{emp.employeeId}</td>
               <td>{emp.name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-export default EmployeeList;
-
-
-  return (
-    <div className="p-4">
-      <h2 className="text-xl font-bold mb-4">Employee List</h2>
-      <table className="w-full border border-collapse border-gray-300">
-        <thead className="bg-gray-100">
-          <tr>
-            <th className="border px-4 py-2">Employee ID</th>
-            <th className="border px-4 py-2">Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employees.map(emp => (
-            <tr key={emp.employeeId}>
-              <td className="border px-4 py-2">{emp.employeeId}</td>
-              <td className="border px-4 py-2">{emp.name}</td>
             </tr>
           ))}
         </tbody>
